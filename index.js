b7f13a16c71a8d49a1ffc4a903314564e7a5a24d
Arthur <lopestui88@gmail.com>
2024-09-10 17:38:51 -0300

    adicionado requirer

---
+++ index.js
@@ -0,0 +1,18 @@
+const start = () => {
+    while(True){
+        let opcao = "sair"
+        switch(opcao){
+            case "cadastrar":
+                console.log("Vamos cadastrar")
+                break;
+            case "listar":
+                console.log("Vamos listar")
+                break;
+            case "sair":
+                return
+
+        }
+    }
+}
+
+start()
