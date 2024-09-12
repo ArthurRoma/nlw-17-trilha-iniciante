f18cf83cef353448c7d237e4209d7c7589fcbfb1
Arthur <lopestui88@gmail.com>
2024-09-12 15:14:17 -0300

    apresenta metas realizadas

--- index.js
+++ index.js
@@ -49,6 +49,23 @@ const listarMetas = async () => {
 }
 
 
+const metasRealizadas = async () => {
+    const realizadas = metas.filter((meta) => {
+        return meta.checked
+
+    })
+
+    if (realizadas.length == 0){
+        console.log("Não existem metas realizadas :(")
+        return 
+    }
+    await select ({
+        message: "Metas realizadas",
+        choices: [...realizadas]
+    })
+    console.log(realizadas)
+}
+
 const start = async () => {
     while(true){
         
@@ -64,6 +81,10 @@ const start = async () => {
                     value: "listar"
                 },
                 {
+                    name: "Metas realizadas",
+                    value: "realizadas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -77,6 +98,9 @@ const start = async () => {
                 break
             case "listar":
                 await listarMetas()
+                break
+            case "realizadas":
+                await metasRealizadas()
                 break
             case "sair":
                 console.log('Até a próxima!')
